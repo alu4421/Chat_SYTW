5d47ebc7b89cb9a24240d84beefcb0068a05baed
Haniel <alu0100721690@ull.edu.es>
2014-11-17 20:35:34 +0000

    Añadiendo label introducir usuario

--- chat.rb
+++ chat.rb
@@ -1,28 +1,38 @@
 require 'sinatra' 
 require 'sinatra/reloader' if development?
-#set :port, 3000
-#set :environment, :production
 
-#chat = ['welcome..']
+set :server, 'thin'
+chat = ['Hola']
 
-get('/') { erb :index }
+
+get('/') do
+  erb :index
+end
 
 get '/send' do
   return [404, {}, "Not an ajax request"] unless request.xhr?
   chat << "#{request.ip} : #{params['text']}"
   nil
 end
-
+#stream(:keep_open) do |out|
+#    settings.connections &lt;&lt; out
+#    out.callback { settings.connections.delete(out) }
+#  end
 get '/update' do
   return [404, {}, "Not an ajax request"] unless request.xhr?
   @updates = chat[params['last'].to_i..-1] || []
-
   @last = chat.size
-  erb <<-'HTML', :layout => false
+  erb <<-"HTML"
       <% @updates.each do |phrase| %>
         <%= phrase %> <br />
       <% end %>
-      <span data-last="<%= @last %>"></span>
+      <span data-last="<%= @last %>"</span>
+
   HTML
 end
+
+#get '/limpiar' do
+#  @updates = []
+#  @last = 0
+#end
 
